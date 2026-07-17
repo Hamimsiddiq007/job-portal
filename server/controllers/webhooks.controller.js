1ab26875e6828e9cd21505b31404974c6d24d4a0
Hamim <hamimsiddiq915@gmail.com>
2026-07-17 20:19:37 +0600

    webhook controller

--- server/controllers/webhooks.controller.js
+++ server/controllers/webhooks.controller.js
@@ -47,6 +47,7 @@ export const clerkWebhooks = async (req, res) => {
                 break;
         }
     } catch (error) {
-        
+        console.log("error", error);
+        res.status(500).json({message: error.message});
     }
 }
