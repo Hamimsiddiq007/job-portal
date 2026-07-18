2c4776125513ba06835ecc578a329bd84ed29593
Hamim <hamimsiddiq915@gmail.com>
2026-07-18 17:36:50 +0600

    company controller

---
+++ server/controllers/company.controller.js
@@ -0,0 +1,39 @@
+// Register new company
+export const registerCompany = async (req, res) => {
+
+}
+
+// Company login
+export const companyLogin = async (req, res) => {
+
+}
+
+// Get company data
+export const getCompanyData = async (req, res) => {
+    
+}
+
+// Post a new job
+export const postJob = async (req, res) => {
+
+}
+
+// Get company job applicants
+export const getJobApplicants = async (req, res) => {
+
+}
+
+// Get company posted jobs
+export const getCompanyJobs = async (req, res) => {
+
+}
+
+// Change job application status
+export const changeApplicationStatus = async (req, res) => {
+
+}
+
+// Change job visibility
+export const changeJobVisibility = async (req, res) => {
+
+}
