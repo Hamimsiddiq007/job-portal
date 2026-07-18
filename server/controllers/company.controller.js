39c00fb1ffce11836fa02476c9908a3c666ce490
Hamim <hamimsiddiq915@gmail.com>
2026-07-18 18:07:11 +0600

    hashed password

--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -1,6 +1,27 @@
+import Company from "../models/company.model.js";
+import bcrypt from "bcrypt";
+
 // Register new company
 export const registerCompany = async (req, res) => {
-    const {name, email, password} = req.body
+    const {name, email, password} = req.body;
+
+    const imageFile = req.file;
+
+    if(!name || !email || !password || !imageFile) {
+        return res.status(400).json({success: false, message: "All fields are required"});
+    }
+
+    try {
+        const companyExists = await Company.findOne({email});
+
+        if(companyExists) {
+            return res.status(400).json({success: false, message: "Company already registered"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+    } catch (error) {
+        
+    }
 }
 
 // Company login
