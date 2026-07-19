e0fbbc872f71e4e78408a9033fa2470a6b241775
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 21:51:52 +0600

    clerk setup

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import companyRoutes from "./routes/company.route.js";
 import connectCloudinary from "./config/cloudinary.js";
 import jobRoutes from "./routes/job.route.js";
 import userRoutes from "./routes/user.route.js";
+import {clerkMiddleware} from '@clerk/express';
 
 // Initialize express
 const app = express();
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 // Routes
 app.get("/", (req, res) => res.send("API Working!"));
