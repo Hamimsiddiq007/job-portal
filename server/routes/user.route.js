8dd0cd9f1ae033e0073a97bd8bc6ff67e996cd8a
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 21:22:04 +0600

    user routes

---
+++ server/routes/user.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { applyForJob, getUserApplications, getUserData, updateUserProfile } from "../controllers/user.controller";
+import upload from "../config/multer";
+
+const router = express.Router();
+
+// Get user data
+router.get("/user", getUserData);
+
+// Apply for a job
+router.post("/apply", applyForJob);
+
+// Get user applications
+router.get("/applications", getUserApplications);
+
+// Update user profile
+router.post("/update-resume", upload.single("resume"), updateUserProfile);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from "./controllers/webhooks.controller.js";
 import companyRoutes from "./routes/company.route.js";
 import connectCloudinary from "./config/cloudinary.js";
 import jobRoutes from "./routes/job.route.js";
+import userRoutes from "./routes/user.route.js";
 
 // Initialize express
 const app = express();
@@ -24,6 +25,7 @@ app.get("/", (req, res) => res.send("API Working!"));
 app.post('/webhooks', clerkWebhooks)
 app.use("/api/company", companyRoutes)
 app.use("/api/jobs", jobRoutes)
+app.use("/api/users", userRoutes)
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
