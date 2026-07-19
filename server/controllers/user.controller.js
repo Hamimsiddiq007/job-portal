5dfd8bee480e835e1d60fa8f5f891ccebe619d14
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 21:14:17 +0600

    user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,13 @@
+
+
+// Get user data
+export const getUserData = async (req, res) => {};
+
+// Apply for a job
+export const applyForJob = async (req, res) => {};
+
+// Get user applications
+export const getUserApplications = async (req, res) => {};
+
+// Update user profile
+export const updateUserProfile = async (req, res) => {};
