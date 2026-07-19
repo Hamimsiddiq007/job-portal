aacb462aea8709848a7349fe8b0e03282918b97c
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 20:43:15 +0600

    getJobById controller

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -2,14 +2,34 @@ import Job from "../models/job.model.js";
 
 // Get all jobs
 export const getAllJobs = async (req, res) => {
-    try {
-        const jobs =await Job.find({isVisible: true}).populate({path: 'companyId', select: '-password'});
+  try {
+    const jobs = await Job.find({ isVisible: true }).populate({
+      path: "companyId",
+      select: "-password",
+    });
 
-        res.status(200).json({ success: true, jobs });
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
-    }
+    res.status(200).json({ success: true, jobs });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Get a single job by ID
-export const getJobById = async (req, res) => {};
+export const getJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const job = await Job.findById(id).populate({
+      path: "companyId",
+      select: "-password",
+    });
+
+    if (!job) {
+      return res.status(404).json({ success: false, message: "Job not found" });
+    }
+
+    res.status(200).json({ success: true, job });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
