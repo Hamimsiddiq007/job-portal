b71ca829a0e6d5bb3d1786b76a35b6ddd4130a8c
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 22:24:52 +0600

    getUserData controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,20 @@
-
+import User from "../models/user.model.js";
 
 // Get user data
 export const getUserData = async (req, res) => {
-    
+    const userId = req.auth.userId;
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        res.status(200).json({ success: true, user });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
 
 // Apply for a job
