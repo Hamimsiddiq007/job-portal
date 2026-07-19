fb94fde980622e3a9eb56870a8abc23da145b85d
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 20:33:50 +0600

    getAllJobs controller

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -1,6 +1,15 @@
+import Job from "../models/job.model.js";
 
 // Get all jobs
-export const getAllJobs = async (req, res) => {};
+export const getAllJobs = async (req, res) => {
+    try {
+        const jobs =await Job.find({visible: true}).populate({path: 'companyId', select: '-password'});
+
+        res.status(200).json({ success: true, jobs });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
 
 // Get a single job by ID
 export const getJobById = async (req, res) => {};
--- server/routes/job.route.js
+++ server/routes/job.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllJobs, getJobById } from '../controllers/job.controller';
+import { getAllJobs, getJobById } from '../controllers/job.controller.js';
 
 const router = express.Router();
 
