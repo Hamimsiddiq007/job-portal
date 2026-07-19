78d465c8094e5ba28f3415b2d5d5bfa4ba434f0e
Hamim <hamimsiddiq915@gmail.com>
2026-07-19 20:12:35 +0600

    job controller

---
+++ server/controllers/job.controller.js
@@ -0,0 +1,6 @@
+
+// Get all jobs
+export const getAllJobs = async (req, res) => {};
+
+// Get a single job by ID
+export const getJobById = async (req, res) => {};
--- server/routes/job.route.js
+++ server/routes/job.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { getAllJobs, getJobById } from '../controllers/job.controller';
 
 const router = express.Router();
 
 // Route to get all jobs data
-
+router.get('/', getAllJobs)
 
 // Route to get a single job by ID
-
+router.get('/:id', getJobById)
 
 export default router;
