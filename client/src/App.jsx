e5074a1110e05e59d75947e1ac5ec8afd129e656
Hamim <hamimsiddiq915@gmail.com>
2026-07-20 23:56:31 +0600

    applied toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import AddJobs from "./pages/AddJobs"
 import ManageJobs from "./pages/ManageJobs"
 import ViewApplications from "./pages/ViewApplications"
 import 'quill/dist/quill.snow.css';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
@@ -18,6 +20,7 @@ const App = () => {
   return (
     <div>
       { showRecruiterLogin && <RecruiterLogin/>}
+      <ToastContainer/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
