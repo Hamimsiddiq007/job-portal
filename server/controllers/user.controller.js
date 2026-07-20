d8aefee89bfda46bdbb7c51026e8472ec6d19932
Hamim <hamimsiddiq915@gmail.com>
2026-07-20 20:09:07 +0600

    getUserApplication controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,53 +4,80 @@ import Job from "../models/job.model.js";
 
 // Get user data
 export const getUserData = async (req, res) => {
-    const userId = req.auth.userId;
+  const userId = req.auth.userId;
 
-    try {
-        const user = await User.findById(userId);
+  try {
+    const user = await User.findById(userId);
 
-        if (!user) {
-            return res.status(404).json({ success: false, message: "User not found" });
-        }
-
-        res.status(200).json({ success: true, user });
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
     }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Apply for a job
 export const applyForJob = async (req, res) => {
-    const { jobId } = req.body;
+  const { jobId } = req.body;
 
-    const userId = req.auth.userId;
-    try {
-        const isApplied = await Application.findOne({jobId, userId});
-        if(isApplied) {
-            return res.status(400).json({ success: false, message: "You have already applied for this job" });
-        }
-
-        const jobData = await Job.findById(jobId);
-        if (!jobData) {
-            return res.status(404).json({ success: false, message: "Job not found" });
-        }
-
-        await Application.create({
-            companyId: jobData.companyId,
-            jobId,
-            userId,
-            date: Date.now()
-        })
-
-        res.status(200).json({ success: true, message: "Application submitted successfully" });
-        
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+  const userId = req.auth.userId;
+  try {
+    const isApplied = await Application.findOne({ jobId, userId });
+    if (isApplied) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "You have already applied for this job",
+        });
     }
+
+    const jobData = await Job.findById(jobId);
+    if (!jobData) {
+      return res.status(404).json({ success: false, message: "Job not found" });
+    }
+
+    await Application.create({
+      companyId: jobData.companyId,
+      jobId,
+      userId,
+      date: Date.now(),
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Application submitted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Get user applications
-export const getUserApplications = async (req, res) => {};
+export const getUserApplications = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+
+    const applications = await Application.find({ userId })
+      .populate("companyId", "name email image")
+      .populate("jobId", "title description location category salary level")
+      .exec();
+
+      if(!applications){
+        return res
+        .status(404)
+        .json({ success: false, message: "No applications found" });
+      }
+
+    res.status(200).json({ success: true, applications });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 // Update user profile
 export const updateUserProfile = async (req, res) => {};
