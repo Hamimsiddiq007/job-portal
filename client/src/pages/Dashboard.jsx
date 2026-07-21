cc16c86e19ba613d372ef87aec40fed6f651ec37
Hamim <hamimsiddiq915@gmail.com>
2026-07-21 21:25:36 +0600

    intigrate companyData

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,20 +1,25 @@
 import { NavLink, Outlet, useNavigate } from "react-router-dom"
 import { assets } from "../assets/assets"
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Dashboard = () => {
 
   const navigate = useNavigate();
 
+  const {companyData} = useContext(AppContext);
+  
   return (
     <div className="min-h-screen">
       {/* Navbar for recruiter panel */}
       <div className="shadow py-4">
         <div className="py-5 flex items-center justify-between">
           <img onClick={() => navigate("/")} className="max-sm:w-32 cursor-pointer" src={assets.logo} alt="" />
+          {companyData && (
           <div className="flex items-center gap-3">
-            <p className="max-sm:hidden">Welcome, Hamim</p>
+            <p className="max-sm:hidden">Welcome, {companyData.name}</p>
             <div className="relative group">
-              <img className="w-8 border border-gray-300 rounded-full" src={assets.company_icon} alt="" />
+              <img className="w-8 border border-gray-300 rounded-full" src={companyData.image} alt="" />
               <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
                 <ul className="list-none m-0 p-2 bg-white rounded-md border border-gray-300 text-sm">
                   <li className="py-1 px-2 cursor-pointer pr-10">Logout</li>
@@ -22,6 +27,7 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
+          )}
         </div>
       </div>
 
