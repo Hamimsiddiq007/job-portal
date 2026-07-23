f01f8c5773af2839e0a2259f80770d822ab41a64
Hamim <hamimsiddiq915@gmail.com>
2026-07-23 19:44:46 +0600

    upload resume

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -50,18 +50,18 @@ const Applications = () => {
         <h2 className='text-xl font-semibold'>Your Resume</h2>
         <div className="flex gap-2 mb-6 mt-3">
           {
-            isEdit ? 
+            isEdit || userData && userData.resume === "" ? 
             <>
               <label className='flex items-center' htmlFor="resumeUpload">
-                <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>Select Resume</p>
+                <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2 cursor-pointer'>{resume ? resume.name : 'Upload Resume'}</p>
                 <input id='resumeUpload' onChange={e => setResume(e.target.files[0])} accept='application/pdf' type="file" hidden />
                 <img src={assets.profile_upload_icon} alt="" />
               </label>
-              <button onClick={() => setIsEdit(false)} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2'>Save</button>
+              <button onClick={() => setIsEdit(false)} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2 cursor-pointer'>Save</button>
             </> 
             :
             <div className='flex gap-2'>
-              <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg' href="#">
+              <a className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg cursor-pointer' href="#">
                 Resume
               </a>
               <button onClick={() => setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer'>Edit</button>
